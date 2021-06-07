5776f816b592b2b6b158f40e06a3436f9fb2c536
stesun2 <ss27@illinois.edu>
2021-06-07 15:34:48 -0500

    work in progress recursion

--- python/recursion_challenge.py
+++ python/recursion_challenge.py
@@ -1,4 +1,14 @@
+# Why is recursion a useful technique for solving a big problem?
+# What are the limitations of using recursive solutions?
+# What types of problems are more suited for simple loops than recursion?
+# What is meant by "recursive depth?"
+# What is a "stack overflow" (the concept, not the website)?
+# Why is that relevant to a recursive problem?
+
+
 def factorial(x):
+
+	# (fact(n-1) + fact (n-2))
 	pass
 
 def palindrome(string):
--- python/recursion_spec.py
+++ python/recursion_spec.py
@@ -1,1 +1,17 @@
 # Write your unit tests here
+import unittest
+from recursion_challenge import recursion_challenge
+
+class RecursionTest(unittest.TestCase):
+
+    def test_factorial():
+        pass
+    
+    def test_palindrome(string):
+	    pass
+
+    def test_bottles(num):
+	    pass
+
+    def test_roman_num(num):
+	    pass
